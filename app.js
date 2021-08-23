dd629f11ebb342ff8af0a2a343ce0fdd51218f3e
Emily81926 <emily81926@gmail.com>
2021-08-24 00:53:05 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000
 
 const mongoose = require('mongoose')
 const db = mongoose.connection
+const exphbs = require('express-handlebars')
 
 mongoose.connect('mongodb://localhost/Expense', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -15,10 +16,13 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('this will be expense tracker!!!')
+  res.render('index')
 })
 
 app.listen(port, () => {
-  console.log(`This is running on a localhost:${port}`)
+  console.log(`This is running on http://localhost:3000`)
 })
