77f61fdbf5d8ea7eb0c5a30e3da04ce8ac4c2c0f
Emily81926 <emily81926@gmail.com>
2021-08-24 20:22:31 +0800

    feat: add delete function

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true}))
 app.get('/', (req, res) => {
   Record.find()
       .lean()
-      .then(records => res.render('index', { records }))
+      .then(record => res.render('index', { record }))
       .catch(error => console.log(error))
   
   
@@ -42,6 +42,39 @@ app.post('/records',(req, res) => {
      .then(() => res.redirect('/'))
      .catch(error => console.log(error))
 })
+
+app.get('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+   .lean()
+   .then((record)=> { res.render('edit', { record })})
+   .catch(error => console.log(error))
+})
+
+app.post('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  const { name, date, category, amount } = req.body
+  return Record.findById(id)
+        .then(record => {
+          record.name = name,
+          record.date = date,
+          record.category = category,
+          record.amount = amount
+          return record.save()
+        })
+        .then(() => res.redirect('/'))
+        .catch((error) => console.log(error))
+})
+
+app.post('/records/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+        .then(record => record.remove())
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+})
+
+
 
 app.listen(port, () => {
   console.log(`This is running on http://localhost:3000`)
