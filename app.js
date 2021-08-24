e7e433e3a1d8236909eb5093a5fd47d765796ba1
Emily81926 <emily81926@gmail.com>
2021-08-24 16:52:35 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3000
 const mongoose = require('mongoose')
 const db = mongoose.connection
 const exphbs = require('express-handlebars')
+const Expense = require('./models/expense')
 
 mongoose.connect('mongodb://localhost/Expense', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -20,21 +21,12 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  //等等要用到mongodb去
-  const list =[{
-    id: 1,
-    name: '午餐' ,
-    date: '2019 / 04 / 23',
-    amount: 60 ,
-    category:'餐飲食品',
-  },{
-    id: 2,
-    name: '早餐',
-    date: '2018 / 03 / 13',
-    amount: 10,
-    category: '餐飲食品',
-  }]
-  res.render('index',{ expense: list })
+  Expense.find()
+      .lean()
+      .then(expenses => res.render('index', { expenses }))
+      .catch(error => console.log(error))
+  
+  
 })
 
 app.listen(port, () => {
